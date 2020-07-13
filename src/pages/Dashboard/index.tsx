532ee6a3f8a1fd249f5912d31061a0b59a38c752
Thiago Bignotto <bignotto@gmail.com>
2020-07-13 00:39:16 -0300

    dashboard ok

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,55 +1,60 @@
-import React from "react";
+import React, { useEffect, useState, FormEvent } from "react";
 import { FiChevronRight } from "react-icons/fi";
+
+import api from "../../services/api";
 
 import { Title, Form, Repositories } from "./styles";
 
 import logoImg from "../../assets/logo.svg";
 
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState("");
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const response = await api.get(`repos/${newRepo}`);
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
+    setNewRepo("");
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer Logo" />
       <Title>Explore Repositórios no Github!</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/2911353?s=460&v=4"
-            alt="Big"
-          />
-          <div>
-            <strong>bignotto/James</strong>
-            <p>James é o robô de relatórios do Big!</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/2911353?s=460&v=4"
-            alt="Big"
-          />
-          <div>
-            <strong>bignotto/James</strong>
-            <p>James é o robô de relatórios do Big!</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
-        <a href="teste">
-          <img
-            src="https://avatars0.githubusercontent.com/u/2911353?s=460&v=4"
-            alt="Big"
-          />
-          <div>
-            <strong>bignotto/James</strong>
-            <p>James é o robô de relatórios do Big!</p>
-          </div>
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
